06c2dd24b3d14dc739b4368c9827b30b27e42c2c
Wilian Morales <willian.moralesch@gmail.com>
2026-03-22 00:31:52 -0500

    feat: se modifico estilos en la vista de information y se agrego el redirect por limites

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, input } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 import { CountryService } from '../../services/country.service';
 import { NotFoundComponent } from "../../../shared/components/not-found/not-found.component";
 import { CountryInformationComponent } from "./country-information/country-information.component";
@@ -11,13 +12,15 @@ import { CountryInformationComponent } from "./country-information/country-infor
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent {
-  countryCode = inject(ActivatedRoute).snapshot.params['code'];
-  countryService = inject(CountryService);
+  private route = inject(ActivatedRoute);
+  private countryService = inject(CountryService);
+
+  countryCode = toSignal(this.route.params.pipe(map(p => p['code'])));
 
   countryResource = rxResource({
-    request: () => ({ code: this.countryCode }),
+    request: () => ({ code: this.countryCode() }),
     loader: ({ request }) => {
       return this.countryService.getCountryByCode(request.code);
     }
-  })
+  });
 }
